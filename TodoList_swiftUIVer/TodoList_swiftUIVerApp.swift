3311d5f7ab0e63560ebc5e7e44ce138ab2ddfb3c
bangtori <58802345+bangtori@users.noreply.github.com>
2023-06-17 00:26:11 +0900

    poject init

---
+++ TodoList_swiftUIVer/TodoList_swiftUIVerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoList_swiftUIVerApp.swift
+//  TodoList_swiftUIVer
+//
+//  Created by 방유빈 on 2023/06/17.
+//
+
+import SwiftUI
+
+@main
+struct TodoList_swiftUIVerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
